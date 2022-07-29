b02efb9aaabee9f6b61f49fc63af4bbbb9e10476
Stephen Wong <wongstephenky@gmail.com>
2022-07-28 21:13:15 -0700

    Added Delete Function to front end

--- src/App.js
+++ src/App.js
@@ -1,20 +1,60 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import TodoForm from "./components/TodoForm";
 import TodoItem from "./components/TodoItem";
 
 function App() {
+  function getFetch() {
+    fetch("/todos")
+      .then((res) => res.json())
+      .then((data) => {
+        setTodoArray(data.items);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }
+  function postTodo(value) {
+    fetch("/todos", {
+      method: "POST",
+      body: JSON.stringify({ label: value }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setTodoArray((val) => [...val, data]))
+      .catch((err) => console.log(err));
+  }
+
+  function delTodo(id) {
+    setLoading(true);
+    fetch(`/todos/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(getFetch())
+      .catch((err) => console.log(err));
+  }
+
+  useEffect(() => {
+    getFetch();
+  }, []);
+
   const initalArrayValue = [];
   const [todoArray, setTodoArray] = useState(initalArrayValue);
+  const [loading, setLoading] = useState(true);
+
   function onAddTask(taskValue) {
     if (taskValue === "") return;
-    setTodoArray([...todoArray, taskValue]);
+    postTodo(taskValue);
   }
   function handleDelItem(id) {
-    setTodoArray((preval) => {
-      return preval.filter((item, index) => index !== id);
-    });
+    console.log(id);
+    delTodo(id);
   }
+
   return (
     <div className="App">
       {/* title */}
@@ -24,14 +64,18 @@ function App() {
       {/* todos */}
       <div className="todo-container">
         <ul>
-          {todoArray.map((todoItem, index) => (
-            <TodoItem
-              text={todoItem}
-              key={index}
-              id={index}
-              del={handleDelItem}
-            />
-          ))}
+          {loading ? (
+            <div>loading</div>
+          ) : (
+            todoArray.map((todoItem) => (
+              <TodoItem
+                text={todoItem.label}
+                key={todoItem.uuid}
+                id={todoItem.uuid}
+                del={handleDelItem}
+              />
+            ))
+          )}
         </ul>
       </div>
     </div>
